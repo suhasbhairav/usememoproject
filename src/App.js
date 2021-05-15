855ed8a96d829c28120eb2f5ef0b7dc050a926ff
Suhas Bhairav <suhasbhairav@gmail.com>
2021-05-15 17:29:45 +0200

    Use Memo
    Use Memo Tutorials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState, useMemo} from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState('');
+
+ 
+    const result = useMemo(() => {
+      return calculateJustNothing(number);
+    }, [number]);   
+
+
+  function calculateJustNothing(number){
+    console.log("test...");
+    let i = 0;
+    while(i<2800000) i++;
+    return Math.random() * number;
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      The square is: {result}
+      <button onClick={() => setNumber( number + 1)}>Increment</button>
+
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <DisplayName name={name} />
     </div>
   );
-}
+};
+
+const DisplayName = React.memo(({name}) => {
+  console.log("inside...");
+  return(
+    <div>The name is: {name}</div>
+  );
+});
 
 export default App;
